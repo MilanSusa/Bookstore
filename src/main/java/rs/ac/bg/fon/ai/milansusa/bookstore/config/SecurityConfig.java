e3bbf413a1313e22bc25aa1cf3c3b334e8f5f5a1
MilanSusa <milan_susa@hotmail.com>
2017-10-20 23:38:09 +0200

    Added unimplemented registration page

--- src/main/java/rs/ac/bg/fon/ai/milansusa/bookstore/config/SecurityConfig.java
+++ src/main/java/rs/ac/bg/fon/ai/milansusa/bookstore/config/SecurityConfig.java
@@ -22,9 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests().antMatchers("/**").authenticated().anyRequest().permitAll().and().formLogin()
-				.loginPage("/login1.html")
-				.loginProcessingUrl("/loginspring")
-				.permitAll().and().logout().permitAll();
+				.loginPage("/login1.html").loginProcessingUrl("/loginspring").permitAll().and().logout().permitAll();
 	}
 
 	@Override
